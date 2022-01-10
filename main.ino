4f7393e919deeca119fc7752f90353285d6eb351
Bandi Jai Krishna <49020643+TextZip@users.noreply.github.com>
2022-01-10 16:47:22 +0530

    initial commit

---
+++ main.ino
@@ -0,0 +1,40 @@
+#define YAW_UPPER_LIMIT   // custom units 
+#define ROLL_UPPER_LIMIT // angle wrt IMU
+#define PITCH_UPPER_LIMIT // angle wrt IMU
+#define SERVO_UPPER_LIMIT // microseconds
+
+#define YAW_LOWER_LIMIT  // custom units
+#define ROLL_LOWER_LIMIT // angle wrt IMU
+#define PITCH_LOWER_LIMIT // angle wrt IMU
+#define SERVO_LOWER_LIMIT // microseconds
+
+#define P_YAW 
+#define I_YAW
+#define D_YAW
+
+#define P_ROLL 
+#define I_ROLL
+#define D_ROLL
+
+#define P_PITCH
+#define I_PITCH
+#define D_PITCH
+
+
+// IMU_CALIBRATION_ROUTINE
+
+// ESC_CALIBRATION_ROUTINE
+
+// SERVO_CALIBRATION_ROUTINE
+
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
